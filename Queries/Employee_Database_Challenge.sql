4c1834f170e35dcc55e614b82f213de29324c2e7
ewaszy <ewa.a.szyszkowska@gmail.com>
2021-08-12 20:31:49 -0500

    Challenge Complete

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -60,6 +60,42 @@ FROM employees as e
 Left outer Join dept_employees as de
 ON (e.emp_no = de.emp_no)
 Left outer Join titles as ti
-ON (e.emp_no = t.emp_no)
+ON (e.emp_no = ti.emp_no)
 WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 ORDER BY e.emp_no;
+
+SELECT * FROM mentorship_eligibilty
+
+-- Three additional queries 
+-- Retrieve number of employees eligible for mentorship program by title
+SELECT COUNT(ut.emp_no), 
+ut.title
+INTO mentorship_count
+FROM mentorship_eligibilty as me
+GROUP BY title 
+ORDER BY COUNT(title) DESC;
+
+-- Employees born in 1964 that can serve as backup mentors 
+SELECT DISTINCT ON(e.emp_no) e.emp_no, 
+    e.first_name, 
+    e.last_name, 
+    e.birth_date,
+    de.from_date,
+    de.to_date,
+    ti.title
+INTO backup_mentors
+FROM employees as e
+Left outer Join dept_employees as de
+ON (e.emp_no = de.emp_no)
+Left outer Join titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1964-01-01' AND '1964-12-31')
+ORDER BY e.emp_no;
+
+-- Number of employees eligible to be a backup mentor by title
+SELECT COUNT(bm.emp_no), 
+bm.title
+INTO backup_mentors_count
+FROM backup_mentors as bm
+GROUP BY title 
+ORDER BY COUNT(title) DESC;
